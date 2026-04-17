dd626f766b60c6612c79ee1aabeeca280aac983b
thisFranklin <bennfranklin01@gmail.com>
2026-04-17 22:16:41 +0100

    Set up a custom odoo image

--- odoo/addons/estate/models/property.py
+++ odoo/addons/estate/models/property.py
@@ -7,12 +7,13 @@ class Property(models.Model):
     _name = 'estate.property'
     _description = 'This is the property model'
 
+
     name = fields.Char(string="Property", required=True)
     description = fields.Text(string="Description", required=True)
     postcode = fields.Char(string="Postal code", required=True)
     date_availability = fields.Date(string="Availability From", required=True)
     expected_price = fields.Float(string="Expected Price", required=True, digits=(6,2))
-    selling_price = fields.Float(string="Selling Price", required=True, digits=(6,2))
+    selling_price = fields.Float(string="Selling Price", required=True, readonly=True, digits=(6,2))
     bedrooms = fields.Integer(string="Number of Bedrooms", required=True, default=2 )
     living_area = fields.Integer(string="Living Area (sqm)", required=True)
     facades = fields.Integer(string="Facades", required=True)
